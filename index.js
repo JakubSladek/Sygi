6e9334dbac15187d2e94fa125a7cde0db1037652
Jakub Sládek <jakub.sladek@protonmail.com>
2020-08-25 16:19:48 +0200

    Now using db as property of client & minor changes

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ require("dotenv").config();
 const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const db = require("quick.db");
 
 client.config = require("./config.json");
 client.dataPath = "./data/users/";
 client.tempMsg = require("./utils/tempMsg");
+client.db = require("quick.db");
 
 //#region Init
 const init = async () => {
@@ -20,64 +20,24 @@ const init = async () => {
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => {
       const folder = dirent.name;
-      const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith(".js"));
-      for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-      }
+      fs.readdirSync(`./commands/${folder}`)
+        .filter((file) => file.endsWith(".js"))
+        .map((file) => {
+          const command = require(`./commands/${folder}/${file}`);
+          client.commands.set(command.name, command);
+        });
     });
   //#endregion
 
   //#region Events
-  const eventFiles = fs.readdirSync(`./events/`).filter((file) => file.endsWith(".js"));
-  for (const file of eventFiles) {
-    const eventName = file.split(".")[0];
-    const event = require(`./events/${file}`);
-    client.on(eventName, event.bind(null, client));
-  }
-  //#endregion
-
-  //#region Store data
-  client.storeUserData = (id, args, callback) => {
-    const dataObj = {
-      warns: 0,
-      kicks: 0,
-      bans: 0,
-    };
-
-    if (args) {
-      if (args.includes("warn")) dataObj.warns++;
-      if (args.includes("kick")) dataObj.kicks++;
-      if (args.includes("ban")) dataObj.bans++;
-    }
-
-    fs.exists(`${client.dataPath}${id}.json`, (exists) => {
-      if (!exists) {
-        fs.writeFile(`${client.dataPath}${id}.json`, JSON.stringify(dataObj, null, " "), (err) => {
-          if (err) throw err;
-          if (callback) callback(dataObj);
-          return;
-        });
-      } else {
-        fs.readFile(`${client.dataPath}${id}.json`, (err, storedData) => {
-          if (err) throw err;
-
-          storedData = JSON.parse(storedData);
-
-          dataObj.warns = storedData.warns + dataObj.warns;
-          dataObj.kicks = storedData.kicks + dataObj.kicks;
-          dataObj.bans = storedData.bans + dataObj.bans;
-
-          fs.writeFile(`${client.dataPath}${id}.json`, JSON.stringify(dataObj, null, " "), (err) => {
-            if (err) throw err;
-            if (callback) callback(dataObj);
-            return;
-          });
-        });
-      }
+  fs.readdirSync(`./events/`)
+    .filter((file) => file.endsWith(".js"))
+    .map((file) => {
+      const eventName = file.split(".")[0];
+      const event = require(`./events/${file}`);
+      client.on(eventName, event.bind(null, client));
     });
-    //#endregion
-  };
+  //#endregion
 
   // Bot login
   client.login(process.env.TOKEN);
